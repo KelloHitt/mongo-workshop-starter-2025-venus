08f74c6ee565fb782fa19911ccd2694230534693
KelloHitt <venus.cert@gmail.com>
2025-08-18 09:42:47 +1200

    Creating a schema for MongoDB database using Mongoose ODM. Created an instance of a schema through mongoose.Schema and adding fields to the schema

--- pokemon-db/src/schema.js
+++ pokemon-db/src/schema.js
@@ -1,3 +1,11 @@
 import mongoose from "mongoose";
 
 /** TODO Your schema here! */
+
+const pokemonSpeciesSchema = new mongoose.Schema({
+    dexNumber: Number,
+    name: String,
+    dexEntry: String,
+
+
+});
